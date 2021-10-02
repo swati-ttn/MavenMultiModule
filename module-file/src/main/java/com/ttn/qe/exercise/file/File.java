99a7e4a5459c21b52b53536f194b91df51899a21
Swati TTN <swatiaswal02@gmail.com>
2021-10-02 15:46:20 +0530

    Multi Module Project

---
+++ module-file/src/main/java/com/ttn/qe/exercise/file/File.java
@@ -0,0 +1,7 @@
+package com.ttn.qe.exercise.file;
+
+public class File {
+        public static void main(String[] args) {
+            System.out.println("File module's File Java file executed");
+        }
+    }
---
+++ module-text/src/main/java/com/ttn/qe/exercise/text/Text.java
@@ -0,0 +1,7 @@
+package com.ttn.qe.exercise.text;
+
+public class Text {
+    public static void main(String[] args) {
+        System.out.println("Text module's Text Java file executed");
+    }
+}
